5b05200387fdc164c952a84ce7a46180c2de88c2
R-Kita <cava.po.09@gmail.com>
2023-09-19 05:59:36 +0000

    add file upload API

--- api/main.py
+++ api/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+# from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
-
 
 @app.get("/hello")
 async def hello():
     return {"message": "hello world!"}
+
+# @app.post("/upload/")
+# async def upload_file(file: UploadFile):
+#     try:
+#         image_data = await file.read()
+#         random_number = (100, 200)
+#         return JSONResponse(content={"random_number": random_number})
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
