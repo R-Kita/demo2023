fbbc5c22e8c903d9c070992636d847a0529c76f6
R-Kita <cava.po.09@gmail.com>
2023-09-19 09:12:48 +0000

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,11 @@
-# python3.9のイメージをダウンロード
 FROM python:3.9-buster
-ENV PYTHONUNBUFFERED=1
 
 WORKDIR /src
 
-# pipを使ってpoetryをインストール
-RUN pip install poetry
+# COPY requirements.txt ./
+# RUN pip install -r requirements.txt
 
-# poetryの定義ファイルをコピー (存在する場合)
-COPY pyproject.toml* poetry.lock* ./
+RUN pip install fastapi uvicorn[standard]
+RUN pip install python-multipart
 
-# poetryでライブラリをインストール (pyproject.tomlが既にある場合)
-RUN poetry config virtualenvs.in-project true
-RUN if [ -f pyproject.toml ]; then poetry install --no-root; fi
-
-# uvicornのサーバーを立ち上げる
-ENTRYPOINT ["poetry", "run", "uvicorn", "api.main:app", "--host", "0.0.0.0", "--reload"]
-
+ENTRYPOINT ["uvicorn", "api.main:app", "--host", "0.0.0.0", "--reload"]
--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-# from fastapi import FastAPI, File, UploadFile
+# from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
@@ -8,13 +8,13 @@ app = FastAPI()
 async def hello():
     return {"message": "hello world!"}
 
-# @app.post("/upload/")
-# async def upload_file(file: UploadFile):
-#     try:
-#         image_data = await file.read()
-#         random_number = (100, 200)
-#         return JSONResponse(content={"random_number": random_number})
-#     except Exception as e:
-#         return JSONResponse(content={"error": str(e)}, status_code=500)
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    try:
+        image_data = await file.read()
+        random_number = (100, 200)
+        return JSONResponse(content={"random_number": random_number})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
