e1aa6ce54517933e33dde07d9edc862096fb2e96
R-Kita <cava.po.09@gmail.com>
2023-09-21 08:56:06 +0000

    add pip libs for RetinaNet

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ WORKDIR /src
 
 RUN pip install fastapi uvicorn[standard]
 RUN pip install python-multipart
+RUN pip install numpy Pillow torch torchvision matplotlib 
 
 ENTRYPOINT ["uvicorn", "api.main:app", "--host", "0.0.0.0", "--reload"]
--- ml_model/retinanet.py
+++ ml_model/retinanet.py
@@ -1,2 +1,12 @@
+import numpy as np
+import torch
+import torchvision
+from torchvision import transforms
+from PIL import Image
+from PIL import ImageDraw, ImageFont
+import matplotlib.pyplot as plt
+from torchvision.models.detection import retinanet_resnet50_fpn
+
 def prediction(image_data):
     return {"retinanet": "test"}
+
